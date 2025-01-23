012fba57ab5dd1f49c5b2a13b2b0b34426faf89c
Evandro Pomatti <evandro.pomatti@gmail.com>
2025-01-23 19:18:57 -0300

    feat: output

--- modules/s3/rclone-bucket/output.tf
+++ modules/s3/rclone-bucket/output.tf
@@ -9,3 +9,7 @@ output "bucket_id" {
 output "bucket_regional_domain_name" {
   value = aws_s3_bucket.main.bucket_regional_domain_name
 }
+
+output "bucket_domain_name" {
+  value = aws_s3_bucket.main.bucket_domain_name
+}
--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
 output "ssm_start_session_command" {
   value = var.ec2_create_instance == true ? "aws ssm start-session --target ${module.instance[0].instance_id}" : null
 }
+
+output "rclone_bucket_domain_name" {
+  value = module.bucket_rclone.bucket_domain_name
+}
